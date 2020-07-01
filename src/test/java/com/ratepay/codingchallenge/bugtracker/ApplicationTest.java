e10956fd9525c1137610525441aebedf817dbc21
Robert Krueger <robert.krueger.ext@ratepay.com>
2020-07-01 13:10:29 +0200

    fix incompatible bytebuddy version, add assertion framework

--- src/test/java/com/ratepay/codingchallenge/bugtracker/ApplicationTest.java
+++ src/test/java/com/ratepay/codingchallenge/bugtracker/ApplicationTest.java
@@ -1,12 +1,19 @@
 package com.ratepay.codingchallenge.bugtracker;
 
 
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import static org.assertj.core.api.Assertions.assertThat;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class ApplicationTest {
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 
+public class ApplicationTest extends AbstractSpringBootTest {
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    @Test
+    public void applicationContextStartsUp() {
+        assertThat(applicationContext).isNotNull();
+    }
 }
